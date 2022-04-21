a0456d8a6b0d1c4352a3fcdf95c08935cecb7ee9
MohamedKhX <muhamedkhx2@gmail.com>
2022-04-21 15:10:53 +0200

    add functions to the  Router

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -5,9 +5,20 @@ namespace Xerophy\Framework\Routing;
 class Router
 {
     /*
-     * The route collection instance
+     * The route collection instance.
      * */
-    public array $routes = [];
+    protected array $routes = [];
+
+    /*
+     * The currently dispatched route instance.
+     * */
+    protected ?Route $route = null;
+
+    /*
+     * The request currently being dispatched.
+     * */
+    protected $currentRequest;
+
 
     /**
      * Add a new GET route
@@ -22,7 +33,68 @@ class Router
        return $this->addRoute($uri, $action, RouteMethod::GET);
     }
 
+    /**
+     * Add a new POST route
+     *
+     * @param string $uri
+     * @param callable|array $action
+     *
+     * @return Route
+     */
+    public function post(string $uri, callable|array $action): Route
+    {
+       return $this->addRoute($uri, $action, RouteMethod::POST);
+    }
 
+    /**
+     * Add a new PUT route
+     *
+     * @param string $uri
+     * @param callable|array $action
+     *
+     * @return Route
+     */
+    public function put(string $uri, callable|array $action): Route
+    {
+        return $this->addRoute($uri, $action, RouteMethod::PUT);
+    }
+
+    /**
+     * Add a DELETE POST route
+     *
+     * @param string $uri
+     * @param callable|array $action
+     *
+     * @return Route
+     */
+    public function delete(string $uri, callable|array $action): Route
+    {
+        return $this->addRoute($uri, $action, RouteMethod::DELETE);
+    }
+
+    public function view()
+    {
+        //TODO implement view later
+    }
+
+    /**
+     * landing to the current uri
+     *
+     * @return ?Route
+     * */
+    public function landing($method, $uri): ?Route
+    {
+        $routes = $this->routes[$method];
+
+        foreach ($routes as $route) {
+            if($uri === $route['uri']) {
+                return $route->run();
+            }
+        }
+
+        return null;
+    }
+    
     /**
      * Register a new Route
      *
