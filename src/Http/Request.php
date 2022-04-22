7e26f288d2f66770fdf19023ebbf5f6e3ce0a2f6
MohamedKhX <muhamedkhx2@gmail.com>
2022-04-22 15:36:14 +0200

    Implment Request

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -1,17 +1,68 @@
 <?php
 
 namespace Xerophy\Framework\Http;
-use Symfony\Component\HttpFoundation\Request as symfonyRequest;
 
-class Request extends symfonyRequest
+
+class Request
 {
-    public function getIp(): ?string
+
+    public function method(): string
     {
-        return $this->getClientIp();
+        return $_SERVER['REQUEST_METHOD'];
     }
 
-    public function Url(): string
+    public function fieldContent(string $fieldName): string|array
     {
-        return $this->getBaseUrl();
+        return $_POST[$fieldName];
+    }
+
+    public function content(): ?array
+    {
+        return $_POST ?? null;
+    }
+
+    public function getUrl()
+    {
+        
+    }
+
+    public function getUrlWithQuery()
+    {
+        
+    }
+
+    public function getFullUrlWithQuery()
+    {
+        
+    }
+
+    public function getFullUrl()
+    {
+        
+    }
+
+    public function getContentType(): ?string
+    {
+        return $_SERVER['CONTENT_TYPE'] ?? null;
+    }
+
+    public function getRequestTime(): ?string
+    {
+        return $_SERVER['REQUEST_TIME'] ?? null;
+    }
+
+    public function getRequestScheme(): ?string
+    {
+        return $_SERVER['REQUEST_SCHEME'] ?? null;
+    }
+
+    public function getHttpHost(): ?string
+    {
+        return $_SERVER['HTTP_HOST'] ?? null;
+    }
+
+    public function getScriptName(): ?string
+    {
+        return $_SERVER['SCRIPT_NAME'] ?? null;
     }
 }
