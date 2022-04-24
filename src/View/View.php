913ecc07f9402c80cb6d3c71696bfcd7155fe926
MohamedKhX <muhamedkhx2@gmail.com>
2022-04-24 14:34:59 +0200

    add view class

--- src/View/View.php
+++ src/View/View.php
@@ -4,5 +4,13 @@ namespace Xerophy\Framework\View;
 
 class View
 {
+    public function render()
+    {
+        $loader = new \Twig\Loader\FilesystemLoader('resources/views/');
+        $twig = new \Twig\Environment($loader, [
+            'cache' => '/path/to/compilation_cache',
+        ]);
 
+        $template = $twig->load('index.html');
+    }
 }
