5c7977468fe541cb4383868c6b80d17744ebd6e6
MohamedKhX <muhamedkhx2@gmail.com>
2022-04-24 23:57:29 +0200

    Implment View class

--- src/View/View.php
+++ src/View/View.php
@@ -4,13 +4,64 @@ namespace Xerophy\Framework\View;
 
 class View
 {
-    public function render()
-    {
-        $loader = new \Twig\Loader\FilesystemLoader('resources/views/');
-        $twig = new \Twig\Environment($loader, [
-            'cache' => '/path/to/compilation_cache',
-        ]);
+    /*
+     * The array of view data
+     * */
+    protected array $params;
 
-        $template = $twig->load('index.html');
+    /*
+     * The path to the view file
+     * */
+    protected string $path;
+
+    /*
+     * The file name
+     * */
+    protected string $fileName;
+
+    /*
+     * The tiwg loader
+     * */
+    protected \Twig\Loader\FilesystemLoader $loader;
+
+    /*
+     * The twig environment
+     * */
+    protected \Twig\Environment $twig;
+
+    /*
+     * Twig options ['https://twig.symfony.com/doc/3.x/api.html']
+     * */
+    protected ?array $twigOptions = [];
+
+    /**
+     * Create a new view instance
+     *
+     * @param string $path
+     * @param string $fileName
+     * @param array  $params
+     * @param array  $twigOptions
+     *
+     * @return void
+     */
+    public function __construct(string $path, string $fileName, array $params = [], $twigOptions = [])
+    {
+        $this->path = $path;
+        $this->fileName = $fileName;
+        $this->params = $params;
+
+        $this->twigOptions = $twigOptions;
+        $this->loader = new \Twig\Loader\FilesystemLoader($this->path);
+        $this->twig = new \Twig\Environment($this->loader, $this->twigOptions);
+    }
+
+    /**
+     * render the view content
+     *
+     * @return void
+     * */
+    public function render(): void
+    {
+        echo $this->twig->render($this->fileName, $this->params);
     }
 }
